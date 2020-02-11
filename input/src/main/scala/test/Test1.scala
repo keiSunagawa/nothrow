f8ac9188b2dc5d6933e7ab84278eddde40c9eb0c
keiSunagawa <keisunagawa.git@icloud.com>
2020-02-11 19:17:45 +0900

    impl Nothrow

--- input/src/main/scala/test/Test1.scala
+++ input/src/main/scala/test/Test1.scala
@@ -5,4 +5,11 @@ package test
 
 object Test1 {
   throw new RuntimeException("ops.") // assert: Nothrow
+
+  Option(null).get // assert: Nothrow
+
+  safeDiv(10, 0).get // assert: Nothrow
+  Right("").right.get // assert: Nothrow
+
+  def safeDiv(i: Int, n: Int) = if (n != 0) Some(i / n) else None
 }
--- rules/src/main/scala/fix/Nothrow.scala
+++ rules/src/main/scala/fix/Nothrow.scala
@@ -1,10 +1,13 @@
 package fix
 
-import metaconfig.Configured
 import scalafix.v1._
 import scala.meta._
 
 class Nothrow extends SemanticRule("Nothrow") {
+  private val optionGet = SymbolMatcher.normalized("scala.Option.get")
+  private val eitherGet =
+    SymbolMatcher.normalized("scala.util.Either.RightProjection.get")
+
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
       case t: Term.Throw =>
@@ -16,6 +19,28 @@ class Nothrow extends SemanticRule("Nothrow") {
               "throw keyword not allowed in this project."
           }
         )
+      case t @ Term.Select(_, n @ Term.Name("get")) =>
+        if (optionGet.matches(n.symbol)) {
+          Patch.lint(
+            new Diagnostic {
+              override def position: _root_.scala.meta.Position = t.pos
+
+              override def message: String =
+                "Option#get not allowed in this project."
+            }
+          )
+        } else if (eitherGet.matches(n.symbol)) {
+          Patch.lint(
+            new Diagnostic {
+              override def position: _root_.scala.meta.Position = t.pos
+
+              override def message: String =
+                "Either#right.get not allowed in this project."
+            }
+          )
+        } else {
+          Patch.empty
+        }
       case _ => Patch.empty
     }.asPatch
   }
